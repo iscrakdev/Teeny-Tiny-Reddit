13f32b536ad8986b5e63d5979fc3a8ddb05566c2
Isaac Rakstad <iscrakdev@gmail.com>
2023-11-16 16:28:35 -0600

    added read more blurbs under each post with no image

--- app.js
+++ app.js
@@ -32,11 +32,11 @@ document.querySelector("#search-bar-submit").addEventListener("click", (e) => {
       // for each post
       for (let child of subreddit.data.children) {
         // get information (author, title, upvotes?, link)
-        // console.log(child.data);
         let author = child.data.author;
         let postURL = "https://www.reddit.com" + child.data.permalink;
         let authorURL = `https://www.reddit.com/user/${author}/`;
         let title = child.data.title;
+        let selfText = child.data.selftext;
 
         const post = document.createElement("li");
         post.classList.add("post-card");
@@ -64,14 +64,25 @@ document.querySelector("#search-bar-submit").addEventListener("click", (e) => {
         post.appendChild(div);
         if (
           child.data.thumbnail !== "self" &&
-          child.data.secure_media === null
+          child.data.secure_media === null &&
+          child.data.thumbnail !== ""
         ) {
           let thumbnail = child.data.thumbnail;
           const thumbnailElement = document.createElement("img");
           thumbnailElement.src = thumbnail;
           post.append(thumbnailElement);
+        } else {
+          const selfTextContainer = document.createElement("p");
+          const link = document.createElement("a");
+          link.classList.add("blue-link");
+          link.target = '_blank'
+          link.innerText = " read more...";
+          link.href = postURL;
+          selfTextContainer.innerText = selfText.slice(0, 100);
+          selfTextContainer.appendChild(link);
+          div.appendChild(selfTextContainer);
         }
-        console.log(post);
+
         ul.appendChild(post);
       }
     });
