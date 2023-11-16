e585810fdc4201af5a3bdc5a42ad1d894fabb84b
Isaac Rakstad <iscrakdev@gmail.com>
2023-11-16 14:41:43 -0600

    made the search bar interactive, user-friendly error if the page does not exist

--- app.js
+++ app.js
@@ -1,43 +1,55 @@
 // addEventListener to run the fetch code
+const errorDiv = document.querySelector('#error-alert')
 
-fetch("https://www.reddit.com/r/javascript.json")
-.catch(err => console.error(err))
-.then(data => data.json())
-.then((data) => {
-    const subreddit = data
-    const ul = document.querySelector('#search-results-list')
-    ul.innerHTML = ''
-    // for each post
-    for (let child of subreddit.data.children) {
+document.querySelector("#search-bar-submit").addEventListener("click", (e) => {
+    e.preventDefault()
+    let subredditName = e.target.parentNode.children[0].value
+    fetch(`https://www.reddit.com/r/${subredditName}.json`)
+    .catch((err) => {
+        document.querySelector('#search-results-list').innerText = `Hmmm, It seems that the subreddit "${subredditName}" does not exist, please try another one?`
+        return
+    })
+    .then((data) => {
+        console.log(data)
+        return data.json()
+    })
+    .then((data) => {
+        console.log(data.data.children.length)
+      const subreddit = data;
+      const ul = document.querySelector("#search-results-list");
+      ul.innerHTML = "";
+      // for each post
+      for (let child of subreddit.data.children) {
         // get information (author, title, upvotes?, link)
-        console.log(child.data)
-        let author = child.data.author
-        let postURL = child.data.url
-        let authorURL = `https://www.reddit.com/user/${author}/`
-        let title = child.data.title
+        // console.log(child.data);
+        let author = child.data.author;
+        let postURL = child.data.url;
+        let authorURL = `https://www.reddit.com/user/${author}/`;
+        let title = child.data.title;
 
-        const post = document.createElement('li')
-        post.classList.add('post-card')
-        
-        const titleElement = document.createElement('h2') 
-        const titleLink = document.createElement('a')
-        
-        const authorElement = document.createElement('p')
-        const authorLink = document.createElement('a')
+        const post = document.createElement("li");
+        post.classList.add("post-card");
 
-        titleLink.href = postURL
-        titleLink.innerText = title
-        titleLink.target = '_blank'
-        titleElement.appendChild(titleLink)
-        
-        authorLink.href = authorURL
-        authorLink.innerText = author
-        authorLink.target = '_blank'
-        authorElement.appendChild(authorLink)
+        const titleElement = document.createElement("h2");
+        const titleLink = document.createElement("a");
 
-        post.appendChild(titleElement)
-        post.appendChild(authorElement)
+        const authorElement = document.createElement("p");
+        const authorLink = document.createElement("a");
 
-        ul.appendChild(post)
-    }
-})
+        titleLink.href = postURL;
+        titleLink.innerText = title;
+        titleLink.target = "_blank";
+        titleElement.appendChild(titleLink);
+
+        authorLink.href = authorURL;
+        authorLink.innerText = author;
+        authorLink.target = "_blank";
+        authorElement.appendChild(authorLink);
+
+        post.appendChild(titleElement);
+        post.appendChild(authorElement);
+
+        ul.appendChild(post);
+      }
+    });
+});
